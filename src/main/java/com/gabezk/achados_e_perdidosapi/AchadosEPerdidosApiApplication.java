8eede928ddf81d48f3db9fb109aaeb4404c8ce0a
Gabriel Fernandes <gabeezk@gmail.com>
2023-12-30 15:03:04 -0300

    Create a project

---
+++ src/main/java/com/gabezk/achados_e_perdidosapi/AchadosEPerdidosApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabezk.achados_e_perdidosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchadosEPerdidosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchadosEPerdidosApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabezk/achados_e_perdidosapi/AchadosEPerdidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabezk.achados_e_perdidosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchadosEPerdidosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
